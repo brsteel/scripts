809ab05d2e0b4e820543e93d4101a44b0eb00b0e
brsteel@microsoft.com <brsteel@microsoft.com>
2024-02-29 18:12:49 -0500

    updates

--- NetworkTests/server.js
+++ NetworkTests/server.js
@@ -1,64 +1,38 @@
+const https = require('https');
 const express = require('express');
 const multer  = require('multer');
 const inMemoryStorage = multer.memoryStorage();
 const upload = multer({ storage: inMemoryStorage });
 const { BlockBlobClient } = require('@azure/storage-blob');
-
+const path = require('path');
 const app = express();
 
 function getAddresses(req) {
     let forwarded = req.headers['x-forwarded-for'];
     let remoteAddress = req.socket.remoteAddress;
-    let response = '';
+    let response = '<h1>Your IP Addresses</h1>';
   
     if (forwarded) {
-      response += 'Forwarded Address: ' + forwarded;
+      response += '<p>Forwarded Address: ' + forwarded + '</p>';
     }
   
     if (remoteAddress) {
-      if (response.length > 0) {
-        response += '<br>';
-      }
-      response += 'Remote Address: ' + remoteAddress;
+      response += '<p>Remote Address: ' + remoteAddress + '</p>';
     }
+
+    response += '<a href="/testconnectivity.ps1.txt">Download testconnectivity.ps1 as text file</a><br>';
+    response += '<a href="/params.json.txt">Download params.json as text file</a>';
   
     return response;
-  }
-  
-  app.get('/', function (req, res) {
+}
+
+app.get('/', function (req, res) {
     var response = getAddresses(req);
     res.send(response);
-  });
-
-  
-  app.put('/upload', upload.single('networktest'), async function (req, res) {
-    const sasUrl = req.query.sasUrl; // Get the SAS URL from the query parameters
-    console.log('sasUrl: ' + sasUrl);
-    console.log('req.file: ' + req.file);
-    if (!sasUrl) {
-        res.status(400).send({
-            message: 'SAS URL is required',
-            sasUrl: sasUrl
-        });
-        return;
-    }
-    const blobName = Date.now() + req.file.originalname;
-
-    // Create a BlockBlobClient directly with the SAS URL and blob name
-    const blockBlobClient = new BlockBlobClient(sasUrl, blobName);
-
-    // Upload the file to Azure Blob Storage
-    const uploadBlobResponse = await blockBlobClient.upload(req.file.buffer, req.file.size);
-    console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
-
-    // Include sasUrl in the response
-    res.send({
-        message: `File uploaded to Azure Blob storage as blob: ${blobName}`,
-        sasUrl: sasUrl
-    });
 });
 
-
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
