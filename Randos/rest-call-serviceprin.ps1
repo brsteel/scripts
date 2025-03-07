49969f64a1f75d6a8cf9d47f82c596576d08e21e
brsteel@microsoft.com <brsteel@microsoft.com>
2025-03-07 10:51:01 -0500

    rest call

--- Randos/rest-call-serviceprin.ps1
+++ Randos/rest-call-serviceprin.ps1
@@ -1,12 +1,12 @@
 # Define the variables
 $tenantId = "3666803f-d1e4-4080-892b-52f70517b979"
 $clientId = "6d2e859d-2bfb-4dce-a1f4-00fc280ee295"
-$clientSecret = "678VEmw-rE0vZ~D4nUV56Hp9T30~O36G~j"
+$clientSecret = "-Kpm58b4F0l8DjRC1Ey-5MyAyx_l1f-fSl"
 $scope = "https://graph.microsoft.us/.default"
 #$graphApiUrl = "https://graph.microsoft.us/v1.0/serviceprincipals
-$graphApiUrl = "https://graph.microsoft.us/v1.0/serviceprincipals?$filter=appId eq '9cdead84-a844-4324-93f2-b2e6bb768d07'"
-#$graphApiUrl = "https://graph.microsoft.us/v1.0/serviceprincipals?$filter=startswith(displayName, 'a')&$count=true&$top=1&$orderby=displayName"
-
+$graphApiUrl = "https://graph.microsoft.us/v1.0/serviceprincipals?`$filter=appId eq '9cdead84-a844-4324-93f2-b2e6bb768d07'"
+#$graphApiUrl = "https://graph.microsoft.us/v1.0/serviceprincipals?`$filter=startswith(displayName, 'a')&$count=true&$top=1&$orderby=displayName"
+#$graphApiUrl = "https://graph.microsoft.us/v1.0/users?`$filter=userPrincipalName eq 'brsteel@fasttrackforazure.onmicrosoft.com'"
 # Get the access token
 $body = @{
     grant_type    = "client_credentials"
@@ -19,7 +19,8 @@ $tokenResponse = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonl
 $accessToken = $tokenResponse.access_token
 
 # Make the API call
-$response = Invoke-WebRequest -Method Get -Uri $graphApiUrl -Headers @{ Authorization = "Bearer $accessToken" }
+#$response = Invoke-WebRequest -Method Get -Uri $graphApiUrl -Headers @{ Authorization = "Bearer $accessToken" }
+$response = Invoke-RestMethod -Method Get -Uri $graphApiUrl -Headers @{ Authorization = "Bearer $accessToken" }
 
 # Output the response
 $response
