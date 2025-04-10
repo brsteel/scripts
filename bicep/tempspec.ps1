aeea3e49d26208613a1e1f1e90f58577abf26b4f
brsteel@microsoft.com <brsteel@microsoft.com>
2025-04-10 12:17:54 -0400

    minor

--- bicep/tempspec.ps1
+++ bicep/tempspec.ps1
@@ -33,7 +33,6 @@ New-AzTemplateSpec -ResourceGroupName $ResourceGroupName -Name $TemplateSpecName
 #avd deployment
 az deployment sub create --name bwsdeployavd --location usgovvirginia --template-file C:\Users\brsteel\Documents\repositories\missionlz\src\bicep\add-ons\azure-virtual-desktop\solution.bicep --parameters C:\Users\brsteel\Documents\repositories\missionlz\src\bicep\add-ons\azure-virtual-desktop\solution.bicepparam --parameters virtualMachineAdminPassword=$password
 
-
 # Prompt the user for a password and wrap it as a secure string
 $sharedkey = Read-Host "Enter your sharedkey" -AsSecureString
 
